57766124d5ff55713f6820cacc2e12fb163d1748
phamquangviet891 <82923647+VietPam@users.noreply.github.com>
2025-03-08 21:57:41 +0700

    init proj

---
+++ src/main/java/com/example/restaurant_simple_api/RestaurantSimpleApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.restaurant_simple_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantSimpleApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantSimpleApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/restaurant_simple_api/RestaurantSimpleApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restaurant_simple_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantSimpleApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
